34bfa93db8057913ac8fa8d6807bbb80c79db4d0
mvdbeek <m.vandenbeek@gmail.com>
2018-07-22 19:08:44 +0300

    Update testing/serving scripts

--- planemo_serve_script.sh
+++ planemo_serve_script.sh
@@ -1,6 +1,1 @@
-planemo serve --galaxy_branch dev \
-    --conda_auto_init \
-    --conda_dependency_resolution \
-    --conda_auto_install \
-    --conda_ensure_channels scrapinghub \
-    --tool_data_table tool_data_table_conf.xml.sample.test "$@"
+planemo serve --tool_data_table tool_data_table_conf.xml.sample.test "$@"
--- test.sh
+++ test.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-planemo test --galaxy_branch dev \
-             --conda_auto_init \
-             --conda_dependency_resolution \
-             --conda_auto_install \
-             --conda_ensure_channels scrapinghub \
-             --tool_data_table tool_data_table_conf.xml.sample.test \
-             "$@"
+planemo test --tool_data_table tool_data_table_conf.xml.sample.test "$@"
